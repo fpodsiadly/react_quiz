3f3f0e77e555247944cced4d4af1ce060264c689
Filip <fpodsiadly@gmail.com>
2025-07-16 14:56:20 +0200

    feat: implement quiz functionality with questions and answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from './components/Header.jsx'
+import Quiz from './components/Quiz.jsx'
 
-export default App;
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  )
+}
+
+export default App
